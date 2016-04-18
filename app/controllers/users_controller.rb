e49aa1b5c4e4f3c82161060694b46c304f44faa7
Caio <caionvivas@yahoo.com.br>
2016-04-17 22:11:58 -0300

    Signup finalizado

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       #Usuario conseguiu criar a conta
+      flash[:success] = "Bem vindo!"
+      redirect_to @user
     else
       #Usuario falhou ao criar conta
       render 'new'
     end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:nome, :cpf, :email, :password, :password_confirmation)
+    end
 end
