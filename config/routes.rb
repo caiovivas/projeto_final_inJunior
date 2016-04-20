29d2a6048300d15c015393f6f454e9b76f1bbd40
Caio_Sanches <caiops143@gmail.com>
2016-04-20 08:21:32 -0300

    criação de tabelas

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
 
+  get 'materials/criarpedido'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
